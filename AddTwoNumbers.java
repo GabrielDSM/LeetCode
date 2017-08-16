ffd34fcb4f5246a364667f7cf7e7a3679db73450
Gabriel Menescal <gabriel.menescal@hotmail.com>
2017-08-16 22:40:26 +0800

    Update to support larger numbers (long)
    Now supports numbers up to Long.MAX_VALUE (2^63 - 1)

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -8,17 +8,17 @@
  */
 
 public class AddTwoNumbers {
-   public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int num1 = parseListNode(l1);
-        int num2 = parseListNode(l2);
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        long num1 = parseListNode(l1);
+        long num2 = parseListNode(l2);
 
-        int sum = num1 + num2;
+        long sum = num1 + num2;
 
         return numberToListNode(sum);
     }
 
-    private static int parseListNode(ListNode root) {
-        int num = root.val;
+    private static long parseListNode(ListNode root) {
+        long num = root.val;
         ListNode temp = root.next;
         int counter = 1;
         while (temp != null) {
@@ -33,21 +33,15 @@ public class AddTwoNumbers {
         return num;
     }
 
-    private static ListNode numberToListNode(int num) {
-        ListNode solution = new ListNode(num % 10);
+    private static ListNode numberToListNode(long num) {
+        ListNode solution = new ListNode((int) (num % 10));
         num = num / 10;
         ListNode temp = solution;
         while (num != 0) {
-            temp.next = new ListNode(num % 10);
+            temp.next = new ListNode((int) (num % 10));
             num = num / 10;
             temp = temp.next;
         }
         return solution;
-    }
-
-    public class ListNode {
-       int val;
-       ListNode next;
-       ListNode(int x) { val = x; }
     }
 }
